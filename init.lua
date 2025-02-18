f0e8656f46b17f9edfd350d8dcbfd52ff868e262
Jonas Pettersen <jonas.pettersen@rvmsystems.com>
2025-02-18 11:31:22 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- Lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({git
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install and configure plugins
+require("vim-options")
+require("lazy").setup("plugins")
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,15 @@
+
+-- Basic settings
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("syntax on")
+vim.cmd("set clipboard=unnamedplus")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
